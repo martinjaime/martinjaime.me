375e1050200982dd24742a84a683b0bcf900d2d0
Martin Jaime <martin.jaime-viveros@originate.com>
2018-10-22 11:26:33 -0700

    setup

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,8 @@ module.exports = {
           {
             loader: "css-loader",
             options: {
-              modules: true
+              modules: true,
+              localIdentName: '[name]_[local]__[hash:base64:5]',
             }
           },
           {
