16abb72565a8f85c9fc8d5c2adc652f3081f8bd3
Martin Jaime <martin.jaime-viveros@originate.com>
2018-11-14 13:38:32 -0800

    update nav menu

--- src/components/NavMenu/NavMenu.js
+++ src/components/NavMenu/NavMenu.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import styles from './NavMenu.scss'
 import classnames from 'classnames/bind'
+import { notifyComingSoon } from '../../helpers/toast'
 
 const cx = classnames.bind(styles)
+const menuItems = [
+  { label: 'about', onClick: notifyComingSoon },
+  { label: 'blog', onClick: notifyComingSoon },
+  { label: 'personal', onClick: notifyComingSoon },
+]
 
 const NavMenu = () => (
-  <div className={cx('nav-menu')}>
-    <span>About</span>
-    <span>Blog</span>
-    <span>Personal</span>
+  <div className={cx('root')}>
+    <div className={cx('nav-menu')}>
+      {menuItems.map(({ label, onClick }) => (
+        <div key={label} onClick={onClick}>
+          {label}
+        </div>
+      ))}
+    </div>
   </div>
 )
 
