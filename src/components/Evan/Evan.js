874ad13ac6516b163507dc4560fd99d63b4d133b
Evan <evanmlongwood@gmail.com>
2019-06-14 15:51:32 -0700

    Added Evan Page.

---
+++ src/components/Evan/Evan.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Evan extends Component {
+  render() {
+    return (
+      <div>
+        <p>yee</p>
+      </div>
+    )
+  }
+}
+
+export default Evan
--- src/components/NavPage/NavPage.js
+++ src/components/NavPage/NavPage.js
@@ -2,25 +2,35 @@ import React from 'react'
 import styles from './NavPage.scss'
 import classnames from 'classnames/bind'
 import { notifyComingSoon } from '../../helpers/toast'
+import PropTypes from 'prop-types'
 
 const cx = classnames.bind(styles)
-const menuItems = [
-  { label: 'about', onClick: notifyComingSoon },
-  { label: 'blog', onClick: notifyComingSoon },
-  { label: 'personal', onClick: notifyComingSoon },
-  { label: 'evan', onClick: },
-]
 
-const NavPage = () => (
-  <div className={cx('root')}>
-    <div className={cx('nav-menu')}>
-      {menuItems.map(({ label, onClick }) => (
-        <div key={label} onClick={onClick}>
-          {label}
-        </div>
-      ))}
+const NavPage = ({ history }) => {
+  const menuItems = [
+    { label: 'about', onClick: notifyComingSoon },
+    { label: 'blog', onClick: notifyComingSoon },
+    { label: 'personal', onClick: notifyComingSoon },
+    { label: 'evan', onClick: () => history.push('/evan') },
+  ]
+
+  return (
+    <div className={cx('root')}>
+      <div className={cx('nav-menu')}>
+        {menuItems.map(({ label, onClick }) => (
+          <div key={label} onClick={onClick}>
+            {label}
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-)
+  )
+}
+
+NavPage.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+}
 
 export default NavPage
