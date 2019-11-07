d8ed33dad3be85f75694d4738400eff469476682
Martin Jaime <martin.jaime-viveros@originate.com>
2019-11-07 11:36:30 -0800

    update rules

--- src/helpers/toast.ts
+++ src/helpers/toast.ts
@@ -1,13 +1,13 @@
-import { toast, ToastId } from 'react-toastify'
+import { toast, ToastId } from 'react-toastify';
 
 export const notify = (message: ToastId) => {
   if (toast.isActive(message)) {
-    return message
+    return message;
   }
 
-  return toast(message, { toastId: message })
-}
+  return toast(message, { toastId: message });
+};
 
 export const notifyComingSoon = () => {
-  notify('Coming soon!')
-}
+  notify('Coming soon!');
+};
